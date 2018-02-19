e6dbcdab48d4e803dce6ae937c7f52f213127e45
vinty1978@gmail.com <djkxtyjr3>
2018-02-19 21:26:04 +0300

    SystemUser

--- web/src/main/java/webApplication/SecurityInitializer.java
+++ web/src/main/java/webApplication/SecurityInitializer.java
@@ -2,8 +2,5 @@
 //
 //import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 //
-///**
-// * Created by User on 29.06.2017.
-// */
 //public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 //}
