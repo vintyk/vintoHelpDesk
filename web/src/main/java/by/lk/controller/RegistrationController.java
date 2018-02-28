4bb7ec87043a0b81cd467d407a3898a516e5c6c1
vinty1978@gmail.com <djkxtyjr3>
2018-02-28 16:22:35 +0300

    контроллер

--- web/src/main/java/by/lk/controller/RegistrationController.java
+++ web/src/main/java/by/lk/controller/RegistrationController.java
@@ -1,6 +1,10 @@
 package by.lk.controller;
 
 import by.lk.dto.SystemUserDto;
+import by.lk.entity.Branch;
+import by.lk.entity.Subdivision;
+import by.lk.repository.BranchRepository;
+import by.lk.repository.SubdivisionRepository;
 import by.lk.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 /**
  * Created by Vinty on 02.07.2017.
  */
@@ -16,16 +22,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class RegistrationController {
 
     private final UserService userService;
+    private final BranchRepository branchRepository;
+    private final SubdivisionRepository subdivisionRepository;
 
     @Autowired
-    public RegistrationController(UserService userService) {
+    public RegistrationController(UserService userService, BranchRepository branchRepository, SubdivisionRepository subdivisionRepository) {
         this.userService = userService;
+        this.branchRepository = branchRepository;
+        this.subdivisionRepository = subdivisionRepository;
     }
 
     @ModelAttribute("systemUsersDto")
     public SystemUserDto systemUsersDto() {
         return new SystemUserDto();
     }
+    @ModelAttribute("branch")
+    public List<Branch> branch() {
+        return branchRepository.findAll();
+    }
+    @ModelAttribute("subdivision")
+    public List<Subdivision> subdivisions() {
+        return subdivisionRepository.findAll();
+    }
+
 
     @GetMapping(path = "/registration")
     public String showRegistrationForm() {
