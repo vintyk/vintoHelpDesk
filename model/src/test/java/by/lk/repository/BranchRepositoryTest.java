45dac9882bbf8ddea08855bbdb7c3949fc57cdeb
vinty <vinty1978@gmail.com>
2018-11-10 17:39:41 +0300

    Вытащить всех по имени. С поиском. проверка на True

---
+++ model/src/test/java/by/lk/repository/BranchRepositoryTest.java
@@ -0,0 +1,15 @@
+package by.lk.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BranchRepositoryTest {
+
+    @Test
+    public void findAllByNameContains() {
+        boolean isTrue = true;
+        Assert.assertTrue(isTrue);
+    }
+}
